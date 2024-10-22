055e20c3712d61c7700107a4d5be7218cc21a08b
Ahmed Anmazir <kyracnh>
2024-10-22 22:01:29 +0100

    nice

--- libft/main.c
+++ libft/main.c
@@ -2,10 +2,5 @@
 
 int main()
 {
-	printf("%d\n", ft_isalpha('j'));
-	printf("%d\n", ft_isalpha('5'));
-	printf("%d\n", ft_isdigit('6'));
-	printf("%d\n", ft_isdigit('j'));
-	printf("%d\n", ft_isalnum('o'));
-	printf("%d\n", ft_isalnum('-'));
+	printf("%d\n", ft_isascii('h'));
 }
