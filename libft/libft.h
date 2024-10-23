200f5470eca04cb07665ed8628dce9e577f64425
Ahmed Anmazir <aanmazir@c3r1p7.1337.ma>
2024-10-23 08:44:04 +0100

    New work

--- libft/libft.h
+++ libft/libft.h
@@ -9,5 +9,7 @@ int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
 size_t	ft_strlen(const char *str);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
 
 #endif
--- libft/main.c
+++ libft/main.c
@@ -2,5 +2,12 @@
 
 int main()
 {
-	printf("%zu\n", ft_strlen("hello"));
+	printf("%c\n", ft_tolower('A'));
+	printf("%c\n", ft_tolower('P'));
+	printf("%c\n", ft_tolower('Z'));
+	printf("%c\n", ft_tolower('R'));
+	printf("%c\n", ft_tolower('L'));
+	printf("%c\n", ft_tolower('O'));
+	printf("%c\n", ft_tolower('G'));
+	printf("%c\n", ft_tolower('E'));
 }
