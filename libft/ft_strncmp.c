9764b18c372a6b7c4327962c6dadf7dafc4d5504
Ahmed Anmazir <kyracnh>
2024-10-24 10:40:39 +0100

    sbah lkhir

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -5,10 +5,13 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
     size_t  i;
 
     i = 0;
-    while (i < n && (s1[i] != '\0' || s2[i] != '\0') && s1[i] == s2[i])
+    while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
     {
-	    return (0);
+        if (s1[i] != s2[i])
+        {
+            return (s1[i] - s2[i]);
+        }
 	    i++;
     }
-    return (s1[i] - s2[i]);
+    return (0);
 }
--- libft/main.c
+++ libft/main.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-	char *s1 = "henlo";
+	char *s1 = "hellp";
 	char *s2 = "hello";
-	size_t n = 4;
+	size_t n = 5;
 
-	printf("%d\n", ft_strncmp(s1, s2, sizeof(n)));
+	printf("%d\n", ft_strncmp(s1, s2, n));
 	
 }
