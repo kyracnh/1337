a55390097921f88a42b6407b363110909e42a147
Ahmed Anmazir <kyracnh>
2024-10-24 16:25:30 +0100

    hylo

--- libft/main.c
+++ libft/main.c
@@ -1,11 +1,12 @@
 #include "libft.h"
+#include <string.h>
 
 int main()
 {
-	char *s1 = "hellp";
-	char *s2 = "hello";
+	char *s1 = "hello";
+	char *s2 = "hell";
 	size_t n = 5;
 
 	printf("%d\n", ft_strncmp(s1, s2, n));
-	
+	printf("%d\n", strncmp(s1, s2, n));
 }
