bcff2fd3f59d720540972c78b597424483ca91df
Ahmed Anmazir <aanmazir@c4r6p1.1337.ma>
2024-10-26 13:10:37 +0100

    Saturday

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -1,27 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aanmazir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 13:07:00 by aanmazir          #+#    #+#             */
+/*   Updated: 2024/10/26 13:10:17 by aanmazir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-size_t  strlcat(char *dest, const char *src, size_t destsize)
+size_t	strlcat(char *dest, const char *src, size_t destsize)
 {
-    size_t  dest_len;
-    size_t  src_len;
-    size_t  i;
+	size_t	dest_len;
+	size_t	src_len;
+	size_t	i;
 
-    dest_len = 0;
-    while (dest_len < destsize && dest[dest_len] != '\0')
-    {
-        dest_len++;
-    }
-    
-    src_len = ft_strlen(src);
-
-    i = 0;
-    while (dest[i] != '\0' && (dest_len + 1) < (destsize - 1))
-    {
-        dest[dest_len + i] = src[i];
-        i++;
-    }
-
-    dest[dest_len+ i] = '\0';
-
-    return (dest_len + src_len);
+	dest_len = 0;
+	while (dest_len < destsize && dest[dest_len] != '\0')
+		dest_len++;
+	src_len = ft_strlen(src);
+	i = 0;
+	while (dest[i] != '\0' && (dest_len + 1) < (destsize - 1))
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+	return (dest_len + src_len);
 }
