95fe7c7a162b1c720bbe126d99bf4c956deffa29
Ahmed Anmazir <aanmazir@c4r6p1.1337.ma>
2024-10-26 16:39:36 +0100

    stage

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: aanmazir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 13:38:36 by aanmazir          #+#    #+#             */
-/*   Updated: 2024/10/26 13:41:25 by aanmazir         ###   ########.fr       */
+/*   Updated: 2024/10/26 16:29:10 by aanmazir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,16 @@
 
 char	*ft_strchr(const char *str, int c)
 {
-	int	i;
+	int		i;
 	char	ch;
 
 	ch = (char) c;
 	i = 0;
 	while (str[i] != '\0')
 	{
-	    if (str[i] == ch)
-	    {
-		return ((char *)str + i);
-	    }
-	    i++;
+		if (str[i] == ch)
+			return ((char *)str + i);
+		i++;
 	}
 	return (0);
 }
--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -1,28 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aanmazir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 16:22:19 by aanmazir          #+#    #+#             */
+/*   Updated: 2024/10/26 16:24:54 by aanmazir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char    *ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-    size_t  i;
-    size_t  j;
+	size_t	i;
+	size_t	j;
 
-    if (needle[0] == '\0')
-    {
-        return ((char *)haystack);
-    }
-
-    i = 0;
-    while (haystack[i] && i < len)
-    {
-        j = 0;
-        while (needle[j] != '\0' && i + j < len && haystack[i + j] == needle[j])
-        {
-            j++;
-        }
-        if (needle[j] == '\0')
-        {
-            return ((char *)haystack + i);
-        }
-        i++;
-    }
-    return (NULL);
+	if (needle[0] == '\0')
+	{
+		return ((char *)haystack);
+	}
+	i = 0;
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (needle[j] != '\0' && i + j < len && haystack[i + j] == needle[j])
+		{
+			j++;
+		}
+		if (needle[j] == '\0')
+		{
+			return ((char *)haystack + i);
+		}
+		i++;
+	}
+	return (NULL);
 }
