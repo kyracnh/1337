0e97e2d3f7a5502afdcbf1043e9c159f3eb8915f
Ahmed Anmazir <aanmazir@c3r10p5.1337.ma>
2024-10-27 20:09:31 +0100

    kol

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -2,5 +2,18 @@
 
 char	*ft_strjoin(char cost *s1, char const *s2)
 {
-	
+	char	*string;
+	int	i;
+	int	l1;
+	int	l2;
+	int	t_len;
+
+	l1 = ft_strlen(s1);
+	l2 = ft_strlen(s2);
+	t_len = l1 + l2;
+	string = (char *)malloc(t_len + 1);
+	if (!string)
+		return (NULL);
+	i = 0;
+
 }
