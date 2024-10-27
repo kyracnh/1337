b79c690866562e94b8d6943c77f47ae0364320d2
Ahmed Anmazir <kyracnh>
2024-10-27 11:22:35 +0100

    hi

--- libft/main.c
+++ libft/main.c
@@ -2,33 +2,8 @@
 
 int main()
 {
-    size_t count = 5;
-    size_t size = sizeof(int);
-    int *arr = (int *)ft_calloc(count, size);
+    char *string = "hello my";
 
-    if (!arr)
-    {
-        printf("Memory allocation failed.\n");
-        return 1;
-    }
-
-    // Check if all elements are zero-initialized
-    int test_passed = 1;
-    for (size_t i = 0; i < count; i++)
-    {
-        if (arr[i] != 0)
-        {
-            printf("Test failed: arr[%zu] is not zero.\n", i);
-            test_passed = 0;
-            break;
-        }
-    }
-
-    if (test_passed)
-    {
-        printf("Test passed: All elements are zero-initialized.\n");
-    }
-
-    free(arr);
+    printf("%ld\n", ft_strlen(string));
     return 0;
 }
