b0882023289aec0cfc4327b765fe42e9692b51b3
Ahmed Anmazir <aanmazir@c4r5p3.1337.ma>
2024-10-28 11:52:28 +0100

    Lundi morning

---
+++ libft/ft_strtrim.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aanmazir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 11:31:07 by aanmazir          #+#    #+#             */
+/*   Updated: 2024/10/28 11:51:58 by aanmazir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int	lset;
+	char	*trimed;
+	int	i;
+
+	lset = ft_strlen(set);
+	string = (char *)malloc(lset + 1);
+	if (!string)
+		return (NULL);
+
+}
