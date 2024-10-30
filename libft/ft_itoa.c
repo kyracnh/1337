47f7589ad431f9c11e01d9fc4571a5105f57b5b0
Ahmed Anmazir <kyracnh>
2024-10-30 11:16:34 +0100

    ft_itoa wa3ra

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -1,6 +1,46 @@
 #include "libft.h"
 
+static int  count_n(int n)
+{
+    int len;
+
+    len = 0;
+    if (n <= 0)
+    {
+        len++;
+        n *= -1;
+    }
+    while (n)
+    {
+        n /= 10;
+        len++;
+    }
+    return (len);
+}
+
 char    *ft_itoa(int n)
 {
-    
+    int len;
+    char    *string;
+    long i;
+
+    len = count_n(n);
+    string = (char *)malloc(sizeof(char) * (len + 1));
+    i = n;
+    if (!string)
+        return (NULL);
+    string[len] = '\0';
+    if (i == 0)
+        string[0] = '0';
+    if (i < 0)
+    {
+        string[0] = '-';
+        i *= -1;
+    }
+    while (i > 0)
+    {
+        string[--len] = (i % 10) + '0';
+        i /= 10;
+    }
+    return (string);
 }
--- libft/main.c
+++ libft/main.c
@@ -2,13 +2,7 @@
 
 int main()
 {
-	char **d = ft_split("Hello my friend how are you doing?", ' ');
+	printf("%s\n", ft_itoa(2147483647));
 
-	for (int i = 0; d[i] != '\0'; i++)
-	{
-		printf("%s\n", d[i]);
-		free(d[i]);
-	}
-	free(d);	
 	return 0;
 }
