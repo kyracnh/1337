33cdb85e78e09c9a1397cc8313b893b405058240
Ahmed Anmazir <kyracnh>
2024-10-31 21:02:48 +0100

    ft_lstadd_back_bonus

--- libft/ft_lstadd_back_bonus.c
+++ libft/ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@ void    ft_lstadd_back(t_list **lst, t_list *new)
 
     if (!lst || !new)
         return ;
-    if (*lst != NULL)
+    if (*lst == NULL)
         *lst = new;
     else
     {
--- libft/main.c
+++ libft/main.c
@@ -18,11 +18,13 @@ int main()
     t_list *node1 = ft_lstnew("First Node");
     t_list *node2 = ft_lstnew("Second Node");
     t_list *node3 = ft_lstnew("Third Node");
+    t_list *node4 = ft_lstnew("ara lakhor");
 
     // Add nodes to the back of the list
     ft_lstadd_back(&head, node1);
     ft_lstadd_back(&head, node2);
     ft_lstadd_back(&head, node3);
+    ft_lstadd_back(&head, node4);
 
     // Print the list to verify the contents
     printf("Linked List after adding nodes to the back:\n");
