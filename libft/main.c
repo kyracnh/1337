0fc6ba7dc95bf2f6c71111b537f386f8a51aee2d
Ahmed Anmazir <kyracnh>
2024-10-31 13:20:33 +0100

    Bonus Part

--- libft/main.c
+++ libft/main.c
@@ -3,16 +3,7 @@
 
 int main()
 {
-	int data = 42;
-    t_list *new_node = ft_lstnew(&data);
-
-    if (new_node)
-        printf("Node content: %d\n", *(int *)new_node->content);
-    else
-        printf("Failed to create new node.\n");
-
-    // Free the node after use to avoid memory leaks
-    free(new_node);
+	
 
     return 0;
 }
