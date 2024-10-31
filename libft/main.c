31b6151a6f61eb7912518ad0954f93427b99113c
Ahmed Anmazir <kyracnh>
2024-10-31 16:25:48 +0100

    ft_lstlast_bonus

--- libft/main.c
+++ libft/main.c
@@ -5,11 +5,17 @@ int main()
 {
 	t_list *head = NULL;
     t_list *node1 = ft_lstnew("Node 1");
+    t_list *node2 = ft_lstnew("Node 2");
+    t_list *node3 = ft_lstnew("Node 3");
+    t_list *node4 = ft_lstnew("good");
 
-    ft_lstadd_front(&head, node1);
+    ft_lstadd_front(&head, node4);
+    ft_lstadd_front(&head, node3); // List: node3 -> NULL
+    ft_lstadd_front(&head, node2); // List: node2 -> node3 -> NULL
+    ft_lstadd_front(&head, node1); // List: node1 -> node2 -> node3 -> NULL
 
-    int size = ft_lstsize(head);  // Call the function to get the list size
-    printf("List size: %d\n", size);  // Expected output: List size: 3
+    t_list *last = ft_lstlast(head);
+    printf("Last node content: %s\n", (char *)last->content);
 
     return 0;
 }
