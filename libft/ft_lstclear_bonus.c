e37494399f2df63ef92b0a6ec200ec8b46538d7c
Ahmed Anmazir <kyracnh>
2024-11-01 10:59:44 +0100

    ft_lstclear_bonus

--- libft/ft_lstclear_bonus.c
+++ libft/ft_lstclear_bonus.c
@@ -2,5 +2,16 @@
 
 void    ft_lstclear(t_list **lst, void (*del)(void *))
 {
-    
+    t_list  *temp;
+
+    if (lst && del)
+    {
+        while (*lst)
+        {
+           temp = (*lst)->next;
+           ft_lstdelone(*lst, del);
+           *lst = temp;
+        }
+        *lst = NULL;
+    }
 }
--- libft/main.c
+++ libft/main.c
@@ -8,7 +8,17 @@ void delete_content(void *content)
 
 int main()
 {
-	
+	t_list *head = ft_lstnew(strdup("Node 1"));
+    ft_lstadd_back(&head, ft_lstnew(strdup("Node 2")));
+    ft_lstadd_back(&head, ft_lstnew(strdup("Node 3")));
+
+    // Clear the list and free all memory
+    ft_lstclear(&head, delete_content);
+
+    if (head == NULL)
+        printf("List successfully cleared.\n");
+    else
+        printf("List clear failed.\n");
 
     return 0;
 }
