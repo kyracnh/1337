be751d3dbf186785244bcdd66aae3052989479a7
Ahmed Anmazir <kyracnh>
2024-11-02 11:00:26 +0100

    test split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -12,58 +12,74 @@
 
 #include "libft.h"
 
-static int	word_count(const char *str, char c) {
-    int	count = 0;
-    int	in_word = 0;
+int	word_c(const char *str, char delim)
+{
+	size_t	count;
 
-    while (*str) {
-        if (*str != c && !in_word) {
-            in_word = 1;
-            count++;
-        } else if (*str == c) {
-            in_word = 0;
-        }
-        str++;
-    }
-    return count;
+	count = 0;
+	while (*str)
+	{
+		if (*str == delim)
+			str++;
+		if (*str)
+		{
+			count++;
+			while (*str && *str != delim)
+				str++;
+		}
+	}
+	return (count);
 }
 
-static char	*word_dup(const char *start, size_t len) {
-    char	*word;
+char	*word_dup(const char *start, size_t len)
+{
+	char	*word;
+	size_t	i;
 
-	word = (char *)malloc(len + 1);
-    if (!word)
-        return NULL;
-    ft_strlcpy(word, start, len + 1);
-    return word;
+	word = (char *)malloc((len + 1) * sizeof(char));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		word[i] = start[i];
+		i++;
+	}
+	word[len] = '\0';
+	return (word);
 }
 
-char	**ft_split(const char *s, char c) {
-    char	**split;
-    size_t	word_c;
-    size_t	i;
-    size_t	len;
+char	**ft_split(const char *s, char del)
+{
+	char		**string;
+	const char	*start;
+	size_t		words;
+	size_t		i;
+	size_t		len;
 
-    if (!s)
-        return NULL;
-
-    word_c = word_count(s, c);
-    split = (char **)malloc((word_c + 1) * sizeof(char *));
-    if (!split)
-        return NULL;
-
-    i = 0;
-    while (*s) {
-        while (*s == c)
-            s++;
-        if (*s) {
-            len = 0;
-            while (s[len] && s[len] != c)
-                len++;
-            split[i++] = word_dup(s, len);
-            s += len;
-        }
-    }
-    split[i] = NULL;
-    return split;
+	if (!s)
+		return (NULL);
+	words = word_c(s, del);
+	string = (char **)malloc(sizeof(char *) * (words + 1));
+	if (!string)
+		return (NULL);
+	i = 0;
+	while (*s)
+	{
+		while (*s == del)
+			s++;
+		if (*s)
+		{
+			start = s;
+			len = 0;
+			while (*s && *s != del)
+			{
+				len++;
+				s++;
+			}
+			string[i++] = word_dup(start, len);
+		}
+	}
+	string[i] = NULL;
+	return (string);
 }
