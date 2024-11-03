f0a2b83af1be6c150d42bd302d3d6b9b06d56f54
Ahmed Anmazir <kyracnh>
2024-11-03 10:16:46 +0100

    done

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -15,15 +15,15 @@
 char	*ft_strchr(const char *str, int c)
 {
 	int		i;
-	char	ch;
-
-	ch = (char) c;
+	
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] == ch)
+		if (str[i] == (char)c)
 			return ((char *)str + i);
 		i++;
 	}
+	if (str[i] == (char)c)
+		return ((char *)str + i);
 	return (0);
 }
