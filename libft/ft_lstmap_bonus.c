f2dbbb682000e676930b53e56479260291f49f30
Ahmed Anmazir <aanmazir@c3r1p9.1337.ma>
2024-11-03 14:42:54 +0100

    Koooool

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,30 +6,32 @@
 /*   By: aanmazir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 21:00:28 by aanmazir          #+#    #+#             */
-/*   Updated: 2024/11/01 21:01:53 by aanmazir         ###   ########.fr       */
+/*   Updated: 2024/11/03 14:34:38 by aanmazir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *)) {
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
 	t_list	*n_list;
 	t_list	*n_elem;
 	void	*new_content;
 
 	n_list = NULL;
-	if (lst && f && del) {
-		while (lst) {
-			// Apply function f to create new content
+	if (lst && f && del)
+	{
+		while (lst)
+		{
 			new_content = f(lst->content);
-			// Create a new list element with the new content
 			n_elem = ft_lstnew(new_content);
-			if (!n_elem) {
-				del(new_content);            // Free new_content if node creation fails
-				ft_lstclear(&n_list, del);   // Clear the entire list
+			if (!n_elem)
+			{
+				del(new_content);
+				ft_lstclear(&n_list, del);
 				return (NULL);
 			}
-			ft_lstadd_back(&n_list, n_elem); // Add the new element to the list
+			ft_lstadd_back(&n_list, n_elem);
 			lst = lst->next;
 		}
 	}
--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,33 +6,39 @@
 /*   By: aanmazir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 14:41:28 by aanmazir          #+#    #+#             */
-/*   Updated: 2024/11/02 13:29:32 by aanmazir         ###   ########.fr       */
+/*   Updated: 2024/11/03 14:38:45 by aanmazir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// Helper function to free allocated memory in case of an error
-static void	free_split(char **split, int words) {
-	for (int i = 0; i < words; i++) {
-		if (split[i]) {
+static void	free_split(char **split, int words)
+{
+	int	i;
+
+	i = 0;
+	while (i < words)
+	{
+		if (split[i])
 			free(split[i]);
-		}
+		i++;
 	}
 	free(split);
 }
 
-// Helper function to count words separated by delimiter
-int	word_c(const char *str, char delim) {
+int	word_c(const char *str, char delim)
+{
 	size_t	count;
 
 	if (!str)
 		return (0);
 	count = 0;
-	while (*str) {
+	while (*str)
+	{
 		while (*str == delim)
 			str++;
-		if (*str) {
+		if (*str)
+		{
 			count++;
 			while (*str && *str != delim)
 				str++;
@@ -41,26 +47,30 @@ int	word_c(const char *str, char delim) {
 	return (count);
 }
 
-// Modified ft_split with malloc checks
-char	**ft_split(const char *s, char del) {
+char	**ft_split(const char *s, char del)
+{
 	char	**string;
-	int		i = 0;
+	int		i;
 	size_t	len;
 
+	i = 0;
 	string = (char **)malloc(sizeof(char *) * (word_c(s, del) + 1));
 	if (!string)
 		return (NULL);
-	while (*s) {
+	while (*s)
+	{
 		while (*s == del && *s)
 			s++;
-		if (*s) {
+		if (*s)
+		{
 			if (!ft_strchr(s, del))
 				len = ft_strlen(s);
 			else
 				len = ft_strchr(s, del) - s;
 			string[i] = ft_substr(s, 0, len);
-			if (!string[i]){  // Check if malloc failed in ft_substr
-				free_split(string, i);  // Free already allocated memory
+			if (!string[i])
+			{
+				free_split(string, i);
 				return (NULL);
 			}
 			i++;
