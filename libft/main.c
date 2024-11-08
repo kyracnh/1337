6cbf84c619e2a1c4901e909eaeabae13f76a65f3
Ahmed Anmazir <kyracnh>
2024-11-08 20:03:40 +0100

    strjoin done

--- libft/main.c
+++ libft/main.c
@@ -4,21 +4,10 @@
 int main()
 {
     char *v = NULL;
-    char *c = "How are you doing ?";
+    char *c = NULL;
 
-    char *r = ft_strjoin(v, c);
-    if (r == NULL)
-    {
-        return 0;
-    }
-    else
-    {
-        for (int i = 0; i < ft_strlen(r); i++)
-        {
-            printf("%c", r[i]);
-        }
-        printf("\n");
-    }
+    ft_strjoin(NULL, 0);
+    
     
     
     return 0;    
