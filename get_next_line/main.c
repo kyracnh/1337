7f92e540c8b29f5e38755b24b749d116c107b7bb
Ahmed Anmazir <aanmazir@c3r5p9.1337.ma>
2024-11-30 12:20:06 +0100

    last

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -4,38 +4,68 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 1024
-
-void *get_line(char *string)
-{
-	int i;
-
-	i = 0;
-	while(string[i] != '\n')
-		write(1, &string[i++], 1);
-}
+#define BUFFER_SIZE 3
 
 int main(void)
 {
+	static char *reserve;
 	char *bo;
-	char buffer[BUFFER_SIZE];
-	int fd = open("ahmed.txt", O_RDONLY);
-	if(fd != -1)
+	char *buffer;
+	char *temp;
+	size_t numRead;
+
+
+	buffer = malloc(BUFFER_SIZE);
+	if(!buffer)
 	{
-		size_t numRead = read(fd, buffer, BUFFER_SIZE-1);
-		while(numRead > 0)
+		free(buffer);
+		return (0);
+	}
+	int fd = open("ahmed.txt", O_RDONLY);
+	if(fd == -1)
+		return (0);
+
+	bo = malloc(100);
+	if(!bo)
+	{
+		free(buffer);
+		return (0);
+		close(fd);
+	}
+	bo[0] = '\0';
+
+	
+	while((numRead = read(fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		temp =malloc(numRead + 1);
+		if(!temp)
 		{
-			buffer[numRead] = '\0';
-			bo = malloc(sizeof(char) * strlen(buffer));
-			bo = buffer;
-			get_line(bo);
-			numRead = read(fd, buffer, BUFFER_SIZE-1);
+			free(buffer);
+			close(fd);
+			return (0);
+		}
+		memcpy(temp, buffer, numRead);
+		temp[numRead] = '\0';
+	
+		if(strchr(temp, '\n') != NULL)
+		{
+			reserve = malloc(numRead + 1);
+			if(!reserve)
+			{
+				free(buffer);
+				free(temp);
+				close(fd);
+				return (0);
+			}
+			strcpy(reserve, temp);
 		}
 	}
-	else
-	{
-		printf("Sorry, can't find this file!\n");
-	}
+
+	strncat(bo, temp, numRead);
+	free(temp);
+	printf("%s\n", bo);
+
+
 	close(fd);
 
 	return 0;
