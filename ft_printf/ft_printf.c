a9a81682ccfefe9a4c379c29de18bd57219dd77a
Ahmed Anmazir <aanmazir@e1r5p12.1337.ma>
2024-12-09 13:54:43 +0100

    Done

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: aanmazir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 09:30:31 by aanmazir          #+#    #+#             */
-/*   Updated: 2024/12/09 09:55:49 by aanmazir         ###   ########.fr       */
+/*   Updated: 2024/12/09 13:35:03 by aanmazir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,31 @@
 
 int	format_sps(va_list args, char format)
 {
-	int	i;
+	int		i;
+	char	*str;
 
 	i = 0;
-			if (format == 'c')
-				i = ft_putchar(va_arg(args, int));
-			else if (format == 's')
-				i = ft_putstr(va_arg(args, char *));
-			else if (format == 'd' || format == 'i')
-			{
-				char	*str;
-
-				str = ft_itoa(va_arg(args, int));
-				i = ft_putstr(str);
-				free(str);
-			}
-			else if (format == 'p')
-				i = ft_putptr(va_arg(args, void *));
-			else if (format == 'u')
-				i = ft_putunbr(va_arg(args, unsigned int));
-			else if (format == '%')
-				i = ft_putchar('%');
-			else if (format == 'x')
-				i = ft_puthex(va_arg(args, unsigned int));
-			else if (format == 'X')
-				i = ft_puthex_u(va_arg(args, unsigned int));
-			return (i);
+	if (format == 'c')
+		i = ft_putchar(va_arg(args, int));
+	else if (format == 's')
+		i = ft_putstr(va_arg(args, char *));
+	else if (format == 'd' || format == 'i')
+	{
+		str = ft_itoa(va_arg(args, int));
+		i = ft_putstr(str);
+		free(str);
+	}
+	else if (format == 'p')
+		i = ft_putptr(va_arg(args, void *));
+	else if (format == 'u')
+		i = ft_putunbr(va_arg(args, unsigned int));
+	else if (format == '%')
+		i = ft_putchar('%');
+	else if (format == 'x')
+		i = ft_puthex(va_arg(args, unsigned int));
+	else if (format == 'X')
+		i = ft_puthex_u(va_arg(args, unsigned int));
+	return (i);
 }
 
 int	ft_printf(const char *format, ...)
@@ -59,20 +58,17 @@ int	ft_printf(const char *format, ...)
 		if (format[i] == '%' && format[i + 1])
 		{
 			i++;
-			o = format_sps(args, format[i]);
-			if ( o == -1)
+			if ((o = format_sps(args, format[i])) == -1)
 				return (-1);
 			len += o;
 		}
 		else
 		{
-			o = write(1, &format[i], 1);
-			if (o == -1)
+			if ((write(1, &format[i], 1)) == -1)
 				return (-1);
 			len++;
 		}
 		i++;
 	}
-	va_end(args);
-	return (len);
+	return (va_end(args), len);
 }
