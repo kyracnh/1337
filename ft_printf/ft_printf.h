c2de32003fb1491a42f8cd8eb415ee0137e40772
Ahmed Anmazir <aanmazir@e1r5p12.1337.ma>
2024-12-10 11:04:27 +0100

    Bismillah ^^

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -22,7 +22,6 @@ int		ft_printf(const char *format, ...);
 int		ft_putchar(char c);
 int	ft_putstr(char *str);
 char	*ft_itoa(int n);
-size_t	ft_strlen(const char *str);
 int		ft_puthex(unsigned long n);
 int		ft_putptr(void *ptr);
 int		ft_putunbr(unsigned int n);
--- ft_printf/main.c
+++ ft_printf/main.c
@@ -2,10 +2,8 @@
 
 int main()
 {
-
-     //int i = ft_printf("hello : %s\n", NULL);
-     //int c = printf("hello : %s\n", NULL);
-     ft_printf(NULL);
-     //ft_printf("i = %d\n", i);
-     //ft_printf("c = %d\n", c);
+     ft_printf("%p\n", NULL);
+     printf("%p\n", NULL);
+     //printf("%p\n", NULL);
+     //printf("%j");
 }
