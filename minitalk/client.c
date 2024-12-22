95511d2ba22cb681866b47641b94434296cc011b
Ahmed Anmazir <aanmazir@c4r2p5.1337.ma>
2024-12-22 13:26:42 +0100

    dona

--- minitalk/client.c
+++ minitalk/client.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -87,3 +88,72 @@ int main(int argc, char *argv[])
 	printf("Server received the message Perfectly 🚀");
 	return (0);
 }
+=======
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aanmazir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 18:35:17 by aanmazir          #+#    #+#             */
+/*   Updated: 2024/12/22 11:11:53 by aanmazir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	send_signal(int pid, char c)
+{
+	int	i;
+	i = 7;
+	while (i >= 0)
+	{
+		int bit;
+
+		bit = (c >> i) & 1;
+		if (bit == 0)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+			{
+				printf("Error sending SIGUSR1\n");
+				exit(1);
+			}
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+			{
+				printf("Error sending SIGUSR2\n");
+				exit(1);
+			}
+		}
+		usleep(60);
+		i--;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int	server_pid;
+	int	i;
+	char	*message;
+
+	if (argc != 3)
+	{
+		printf("Use : %s <server_pid> \"message\"\n", argv[0]);
+		return (1);
+	}
+	server_pid = atoi(argv[1]);
+	message = argv[2];
+	i = 0;
+	while (message[i] != '\0')
+	{
+		send_signal(server_pid, message[i]);
+		printf("Signal %i has been sent perfectly 🚀\n", i);
+		i++;
+	}
+	send_signal(server_pid, '\0');
+	return (0);
+}
+>>>>>>> da0f36e (solo)
--- minitalk/server.c
+++ minitalk/server.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -65,3 +66,61 @@ int main(void)
 		pause();
 	return (0);
 }
+=======
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aanmazir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 13:55:22 by aanmazir          #+#    #+#             */
+/*   Updated: 2024/12/22 11:21:47 by aanmazir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	handle_signal(int signal)
+{
+	static char	current_char = 0;
+	static int	current_bit = 0;
+
+	current_char <<= 1;
+	if (signal == SIGUSR1)
+		current_char |= 0;
+	else if (signal == SIGUSR2)
+		current_char |= 1;
+	current_bit++;
+	if (current_bit == 8)
+	{
+		write(1, &current_char,1);
+		current_bit = 0;
+		current_char = 0;
+	}
+}
+
+int main(void)
+{
+	struct sigaction sa;
+
+	sa.sa_handler = handle_signal;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+	{
+		ft_printf("Error Regestering SIGUSR1 handler");
+		return (1);
+	}
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+	{
+		ft_printf("Error registering SIGUSR2 handler");
+		return (1);
+	}
+	ft_printf("Server PID 📡: %d\n", getpid());
+	while (1)
+	{}
+	return (0);
+}
+>>>>>>> da0f36e (solo)
